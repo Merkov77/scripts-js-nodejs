74ded2409f8205c6abe9c5421c36996205007afa
José Mavo <34657109+Merkov77@users.noreply.github.com>
2020-03-27 10:11:03 -0400

    Update tejs.js

--- typeEfectjs/tejs.js
+++ typeEfectjs/tejs.js
@@ -1,53 +1,29 @@
 let root = document.getElementById("root")
 
 
-
 function typeWrite(text, time){
 
 	let count = 0; let interval1, interval2;
-
 	let mess = text
-
-
-
+	
 	let interval = setInterval(()=>{
-
 		if (count < mess.length){
-
 			if (mess[count-1] != "," && mess[count-1] != "." ){
-
 				if (mess[count] != undefined){
-
 					root.textContent += mess[count++] 	
-
 				}
-
 			} else{
-
 				setTimeout(()=>{
-
 					if (mess[count] != undefined){
-
 						root.textContent += mess[count++] 	
-
 					}
-
 				}, 500)
-
 			}
-
-
-
 		} else {
-
 			clearInterval(interval)
-
 			console.log("End")
-
 		}	
-
 	},time)
-
 }
 
 
